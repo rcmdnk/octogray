f3539101caa90b19a987f539fd405db7d739cc6f
rcmdnk <rcmdnk@gmail.com>
2014-07-09 14:08:42 +0900

    removed puts from post.rb

--- plugins_add/post.rb
+++ plugins_add/post.rb
@@ -4,12 +4,15 @@ module Jekyll
   class Post
     # Categories
     def populate_categories
-      puts "in plugin!"
       categories_from_data = Utils.pluralized_array_from_hash(data, 'category', 'categories')
       self.categories = (
         Array(categories) + categories_from_data
-      ).map {|c| c.to_s.capitalize}.flatten.uniq
-      #)
+      ).map {|c| c.to_s.capitalize}.flatten.uniq # Capitalize categories
+      #) # Don't change (Be careful, must use exactly same capitalization for same category name
+         # e.g. if one post has "Category" and other has "category",
+         # there is only one "Category" or "category"
+         # and one of them is ignored.
+      #).map {|c| c.to_s.downcase}.flatten.uniq # All downcase (default)
     end
 
     # Tags
