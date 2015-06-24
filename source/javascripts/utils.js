05235fe8fbbf2182f8a7548dff5140a2d53450ac
rcmdnk <rcmdnk@gmail.com>
2015-06-24 12:33:12 +0900

    remoed ios bug fix from utils.js

--- source/javascripts/utils.js
+++ source/javascripts/utils.js
@@ -1,24 +1,3 @@
-// iOS scaling bug fix
-// Rewritten version
-// By @mathias, @cheeaun and @jdalton
-// Source url: https://gist.github.com/901295
-$(function(){
-  var addEvent = 'addEventListener',
-      type = 'gesturestart',
-      qsa = 'querySelectorAll',
-      scales = [1, 1],
-      meta = qsa in document ? document[qsa]('meta[name=viewport]') : [];
-  function fix() {
-    meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
-    document.removeEventListener(type, fix, true);
-  }
-  if ((meta = meta[meta.length - 1]) && addEvent in document) {
-    fix();
-    scales = [0.25, 1.6];
-    document[addEvent](type, fix, true);
-  }
-});
-
 $(function(){
   $(".add_bookmark").click(function () {
   if ( ! $(this).data('page_title') || ! $(this).data('page_url') ){
